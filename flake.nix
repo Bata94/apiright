ba4ce10ce3ad058d17a97f45760ab085775a0ab5
Bastian Sievers <b.sievers@sits.ruhr>
2025-05-24 23:58:51 +0200

    curr state

---
+++ flake.nix
@@ -0,0 +1,32 @@
+{
+  description = "ApiRight Dev Shell";
+
+  inputs = {
+    nixpkgs.url = "nixpkgs/nixos-unstable";
+    flake-utils.url = "github:numtide/flake-utils";
+  };
+
+  outputs = { self, nixpkgs, flake-utils, ... }:
+    flake-utils.lib.eachDefaultSystem (system:
+      let
+        pkgs = nixpkgs.legacyPackages.${system};
+      in {
+        devShell = pkgs.mkShell {
+          buildInputs = with pkgs; [
+            just
+            go
+            air
+            # sqlc
+            # goose
+
+            wrk
+          ];
+          shellHook = ''
+            go mod tidy
+          '';
+        };
+      });
+}
+
+
+
--- go.mod
+++ go.mod
@@ -1,3 +1,3 @@
-module github.com/bata94/go_fastapilike
+module github.com/bata94/apiright
 
 go 1.23.4
