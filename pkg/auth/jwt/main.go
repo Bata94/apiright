30ddf52f6cdf3be163fe270a7f30906652794c01
bata94 <bastian.sievers@gmail.com>
2025-10-17 14:08:09 +0200

    changed JWT approach a bit (WIP)

--- pkg/auth/jwt/main.go
+++ pkg/auth/jwt/main.go
@@ -1,77 +1,54 @@
-
 package jwt
 
 import (
-	"errors"
-	"fmt"
 	"time"
 
-	"github.com/bata94/apiright/pkg/core"
+	ar "github.com/bata94/apiright/pkg/core"
 	"github.com/golang-jwt/jwt/v5"
 )
 
-var (
-	ErrJWTMissing = errors.New("missing or malformed JWT")
-	ErrJWTInvalid = errors.New("invalid or expired JWT")
-)
+type SigningMethod = jwt.SigningMethod
+type Claims = jwt.Claims
 
 type JWTConfig struct {
-	Secret        []byte
-	SigningMethod jwt.SigningMethod
-	Expiration    time.Duration
-	ContextKey    string
+	Issuer string
+	Secret string
+	SigningMethod SigningMethod
+	TTL time.Duration
+	TTLRefreshToken time.Duration
+	// MaxRefreshTokenAge is the maximum age of a refresh token.
+	// If set to 0, RefreshToken will live forever.
+	MaxRefreshTokenAge time.Duration
+
+	AdditionalClaimsFunc func(ar.Ctx) Claims
 }
 
-func NewJWTConfig(secret []byte) JWTConfig {
-	return JWTConfig{
-		Secret:        secret,
-		SigningMethod: jwt.SigningMethodHS256,
-		Expiration:    time.Hour * 24,
-		ContextKey:    "user",
+var (
+  key []byte
+  t   *jwt.Token
+  s   string
+)
+
+func init() {
+  key = []byte("secret")
+
+	t = jwt.NewWithClaims(jwt.SigningMethodES256,
+		jwt.MapClaims{
+			"iss": "my-auth-server",
+			"sub": "john",
+			"foo": 2,
+		})
+
+	s, err := t.SignedString(key)
+	if err != nil {
+		panic(err)
 	}
 }
 
-func (c *JWTConfig) GenerateToken(claims jwt.Claims) (string, error) {
-	token := jwt.NewWithClaims(c.SigningMethod, claims)
-	return token.SignedString(c.Secret)
-}
-
-func (c *JWTConfig) Middleware() core.Middleware {
-	return func(next core.Handler) core.Handler {
-		return func(ctx *core.Ctx) error {
-			authHeader := ctx.Request.Header.Get("Authorization")
-			if authHeader == "" {
-				return ErrJWTMissing
-			}
-
-			tokenString := ""
-			count, err := fmt.Sscanf(authHeader, "Bearer %s", &tokenString)
-			if tokenString == "" {
-				return ErrJWTMissing
-			} else if count != 1 {
-				return ErrJWTInvalid
-			} else if err != nil {
-				return ErrJWTInvalid
-			}
-
-			token, err := jwt.Parse(tokenString, func(token *jwt.Token) (any, error) {
-				if _, ok := token.Method.(*jwt.SigningMethodHMAC); !ok {
-					return nil, fmt.Errorf("unexpected signing method: %v", token.Header["alg"])
-				}
-				return c.Secret, nil
-			})
-
-			if err != nil {
-				return ErrJWTInvalid
-			}
-
-			if claims, ok := token.Claims.(jwt.MapClaims); ok && token.Valid {
-				ctx.Session[c.ContextKey] = claims
-			} else {
-				return ErrJWTInvalid
-			}
-
-			return next(ctx)
+func JWTMiddleware(config JWTConfig) ar.Middleware {
+	return ar.Middleware(func(next ar.Handler) ar.Handler {
+		return func(c *ar.Ctx) error {
+			return next(c)
 		}
-	}
+	})
 }
